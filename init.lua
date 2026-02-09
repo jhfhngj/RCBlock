28309b0c0358fc6e3479e05ec266567ad6a61102
Ezzeldin Ahmed <99299697+jhfhngj@users.noreply.github.com>
2026-02-09 23:21:36 +0000

    Fix init.lua

--- init.lua
+++ init.lua
@@ -1,14 +1,56 @@
-minetest.register_node("rcblock:rc_block", {
-    description = "Remote Control Block",
-    tiles = {"rcblock.png"},
-    after_place_node = function(pos,placer)
+local rc_registry = {}
+
+minetest.register_node("rcblock:controller", {
+    description = "RC Block",
+    tiles = {"rcblock_controller.png"},
+    sounds = default.node_sound_metal_defaults(),
+
+    after_place_node = function(pos, placer)
         local meta = minetest.get_meta(pos)
-        local secret = tostring(minetest.get_us_time())
-        local toSend =  "Hello dear player! Your Control ID is '" .. secret .. "'. Keep this safe unless you want other players using your block!"
-        meta:set_int("id",secret)
-        minetest.chat_send_player(placer:get_player_name(),toSend)
-        minetest.get_node_timer(pos):start(0.1)  -- runs every 100 ms
+        local id = tostring(minetest.get_us_time())  -- unique ID
+        meta:set_string("id", id)
+
+        rc_registry[id] = pos
+        minetest.chat_send_player(placer:get_player_name(),
+            "RC Block placed with ID: " .. id)
     end,
-    groups = {machine = 5, misc = 1},
-    sounds = default.node_sound_metal_defaults()
+
+    after_dig_node = function(pos, oldnode, oldmeta, digger)
+        local id = oldmeta:get_string("id")
+        rc_registry[id] = nil
+    end,
+})
+minetest.register_chatcommand("rc", {
+    params = "<id> <dir>",
+    description = "Move an RC block",
+    func = function(name, param)
+        local id, dir = param:match("^(%S+)%s+(%S+)$")
+        if not id or not dir then
+            return false, "Usage: /rc <id> <X+/X-/Y+/Y-/Z+/Z->"
+        end
+
+        local pos = rc_registry[id]
+        if not pos then
+            return false, "No RC block with ID " .. id
+        end
+
+        local new = vector.new(pos)
+
+        if dir == "X+" then new.x = new.x + 1 end
+        if dir == "X-" then new.x = new.x - 1 end
+        if dir == "Y+" then new.y = new.y + 1 end
+        if dir == "Y-" then new.y = new.y - 1 end
+        if dir == "Z+" then new.z = new.z + 1 end
+        if dir == "Z-" then new.z = new.z - 1 end
+
+        -- Move the block
+        local node = minetest.get_node(pos)
+        minetest.set_node(pos, {name="air"})
+        minetest.set_node(new, node)
+
+        -- Update registry
+        rc_registry[id] = new
+
+        return true, "Moved RC block " .. id .. " to " .. minetest.pos_to_string(new)
+    end
 })
